58b8d1e26759aced08da6d107277738af64b1f65
saf3 <saf3@CSG21-40.cs.ucc.ie>
2013-02-26 14:05:58 +0000

    More netcode.

--- src/net/Connector.java
+++ src/net/Connector.java
@@ -1,6 +1,10 @@
 package net;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.MessageDigest;
+import log.ErrorLogger;
 
 /**
  * Connects to the server backend and tries to access it through the
@@ -9,10 +13,12 @@ import java.net.Socket;
  * @author Stephen Fahy
  */
 public class Connector {
-    private static final String DEFAULT_URL = "";
+    private static final String DEFAULT_IP = "localhost";
     private static final int DEFAULT_AUTH_PORT = 579;
     private static Socket SOCKET;
-    
+    private static ObjectInputStream IN;
+    private static ObjectOutputStream OUT;
+    private static final int TIMEOUT = 500000;
     
     /**
      * Tries to connect to the server with the given details.
@@ -24,7 +30,44 @@ public class Connector {
     private static boolean connect(String username, String password) {
         boolean output = false;
         
-        
+        try {
+            Socket authSocket = new Socket(DEFAULT_IP, DEFAULT_AUTH_PORT);
+            ObjectInputStream authIn = new ObjectInputStream(authSocket.getInputStream());
+            ObjectOutputStream authOut = new ObjectOutputStream(authSocket.getOutputStream());
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes("UTF-8"));
+            byte[] digest = md.digest();
+            String passhash = new String(digest);
+            AuthMessage auth = new AuthMessage(0, username, passhash);
+            int timer = 0;
+            
+            auth.send(authOut);
+            
+            while(authIn.available() == 0 && timer < TIMEOUT) {
+                //wait until a reply from the server is received or timeout
+                timer++;
+            }
+            
+            Message reply = (Message) authIn.readObject();
+            
+            if(reply instanceof ConnectionMessage) {
+                //setup socket
+                
+                IN = new ObjectInputStream(SOCKET.getInputStream());
+                OUT = new ObjectOutputStream(SOCKET.getOutputStream());
+            }
+            else if(reply instanceof AckMessage) {
+                //read nack
+            }
+            else {
+                ErrorLogger.get().log("Unknown message received from server: " +
+                        reply.toString());
+            }
+        }
+        catch(Exception e) {
+            ErrorLogger.get().log(e.toString());
+            e.printStackTrace();
+        }
         
         return output;
     }
@@ -32,5 +75,11 @@ public class Connector {
     //getters
     public static Socket getSocket() {
         return SOCKET;
+    }
+    public static ObjectInputStream getIn() {
+        return IN;
+    }
+    public static ObjectOutputStream getOut() {
+        return OUT;
     }
 }
